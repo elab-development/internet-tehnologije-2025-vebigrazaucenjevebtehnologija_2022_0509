58c5949375278d8a6008f0b7f58b2130a4d4d306
jozefkis <jozefkis303@gmail.com>
2026-02-09 23:00:54 +0100

    Import Login to App

--- grid-chess-front/src/App.tsx
+++ grid-chess-front/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login'; 
 
 function App() {
+  // Stanje za token koje Login komponenta zahteva
+  const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {}
+          <Route 
+            path="/login" 
+            element={<Login setToken={(t: string) => setToken(t)} />} 
+          />
+          
+          <Route 
+            path="/" 
+            element={<Navigate to="/login" />} 
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
