dd905cbf3fd149aabf43a55eff2914e767df471a
Jozef <jozefkis303@gmail.com>
2026-03-11 21:20:46 +0100

    dodata provera za guest-mode u axiosu i sredjen index.tsx

--- grid-chess-front/src/api/axios.ts
+++ grid-chess-front/src/api/axios.ts
@@ -6,10 +6,16 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
+  
+  if (token && token !== 'guest-mode') {
+    if (config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
+  
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
